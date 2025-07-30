f173ec10fd21e2e1d4e125aa3d0d1482bddad0b3
iandtsoft-27452103 <takamune-iwasaki@iandtsoft.com>
2025-07-30 10:35:14 +0900

    Add files via upload

---
+++ Achernar/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq.Expressions;
+using System.Runtime.Intrinsics;
+using System.Transactions;
+
+namespace Achernar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test t = new Test();
+            //t.TestMakeMove();
+            int task_num, thinking_time;
+            bool is_console_out;
+            Common.Init();
+            Hash.IniRand(5489U);
+            Hash.IniRandomTable();
+
+            string[] str_header = new string[9];
+            str_header[0] = args[0];
+            str_header[1] = args[1];
+            str_header[2] = args[2];
+            str_header[3] = args[3];
+            str_header[4] = args[4];
+            str_header[5] = args[5];
+            str_header[6] = args[6];
+            str_header[7] = args[7];
+            str_header[8] = args[8];
+
+            switch (str_header[0])
+            {
+                case "a":
+                    task_num = int.Parse(args[5]);
+                    thinking_time = int.Parse(args[6]);
+                    Analyze.AnalyzeRecord(str_header[3], str_header[4], task_num, str_header, thinking_time);
+                    break;
+                case "s":
+                    task_num = int.Parse(args[5]);
+                    int game_num = int.Parse(args[7]);
+                    thinking_time = int.Parse(args[6]);
+                    is_console_out = bool.Parse(args[8]);
+                    SelfPlay sp = new SelfPlay();
+                    sp.SelfPlayWrapper(task_num, game_num, thinking_time, is_console_out);
+                    break;
+            }
+
+
+            /*string[] str_header = new string[4];
+            str_header[0] = "2022/04/03";
+            str_header[1] = "第70回NHK杯1回戦";
+            str_header[2] = "鈴木歩七段";
+            str_header[3] = "沼舘沙輝哉七段";
+            Analyze.AnalyzeRecord("20220403_nhk_hai.txt", "analyze_result.txt", 1, str_header, 10);*/
+
+            /*str_header[0] = "2022/09/04";
+            str_header[1] = "第70回NHK杯2回戦";
+            str_header[2] = "羽根直樹九段";
+            str_header[3] = "林漢傑八段";
+            Analyze.AnalyzeRecord("20220904_nhk_hai.txt", "analyze_result.txt", 3, str_header, 3);*/
+
+            /*str_header[0] = "2022/10/30";
+            str_header[1] = "第70回NHK杯2回戦";
+            str_header[2] = "井山裕太名人";
+            str_header[3] = "張栩九段";
+            Analyze.AnalyzeRecord("20221030_nhk_hai.txt", "analyze_result.txt", 3, str_header, 3);*/
+        }
+    }
+}
---
+++ Achernar/Test.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Achernar.MakeMove;
+
+namespace Achernar
+{
+    internal class Test
+    {
+        public void TestMakeMove()
+        {
+            Common.Init();
+            Hash.IniRand(5489U);
+            Hash.IniRandomTable();
+            List<Record> records = new List<Record>();
+            records = IO.ReadRecordFile("test_records.txt");
+            List<int> li = new List<int>();
+
+            for (int i = 0; i < records.Count; i++)
+            {
+                if (i == 14 || records[i].str_moves.Length > 250)
+                {
+                    string s = "record_no = " + (i+1).ToString();
+                    s += ", ●" + records[i].players[0];
+                    s += ", ○" + records[i].players[1];
+                    Console.WriteLine(s);
+                    li.Add(i);
+                }
+            }
+
+            Board bt = new Board();
+            short color;
+            for (int i = 0;i < li.Count;i++)
+            {
+                Record record = records[li[i]];
+                bt.Init();
+                color = 0;
+                for (short j = 0; j < record.moves.Length; j++)
+                {
+                    short move = record.moves[j];
+
+                    if (i == 12 && j == 297)
+                    {
+                        int a = 0;// トリによって自分の駄目を復活させる処理から再開する
+                    }
+
+                    Do(ref bt, move, color, (short)(j + 1));
+                    if (i == 12 && j == 297)
+                    {
+                        UnDo(ref bt, move, color, (short)(j + 1));
+                        OutBoard(bt);
+
+                        /*int cnt = 0;
+                        for (int k = 0; k < 256; k++)
+                        {
+                            if (bt.dame_sq[1, k].Contains(120))
+                            {
+                                cnt++;
+                            }  
+                        }
+                        for (int k = 0; k < bt.dame_sq[1,61].Count; k++)
+                        {
+                            Console.Write(bt.dame_sq[1, 61][k]);
+                            Console.Write(",");
+                        }*/
+                        //Console.WriteLine(cnt);
+                        return;
+                    }
+                    color ^= 1;
+                }
+            }
+        }
+
+        private void OutBoard(Board bt)
+        {
+            string str_out = "";
+            int cnt = 0;
+            for (int i = 0; i < Common.NSquare; i++)
+            {
+                switch(bt.board[i])
+                {
+                    case 0:
+                        str_out += "○ ";
+                        break;
+                    case 1:
+                        str_out += "● ";
+                        break;
+                    case 2:
+                        str_out += "+ ";
+                        break;
+                }
+                cnt++;
+                if (cnt == Common.NSide)
+                {
+                    str_out += "\n";
+                    cnt = 0;
+                }
+            }
+
+            str_out = str_out + "\n";
+            str_out += "黒のアゲハマ" + bt.agehama[0].ToString() + "\n";
+            str_out = str_out + "\n";
+            str_out += "白のアゲハマ" + bt.agehama[1].ToString() + "\n";
+
+            Console.WriteLine(str_out);
+        }
+    }
+}
